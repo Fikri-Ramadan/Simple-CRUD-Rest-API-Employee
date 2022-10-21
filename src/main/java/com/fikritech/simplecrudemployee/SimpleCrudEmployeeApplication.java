41b8fe1c7b169596d68202a79c30c21d398d301e
FikriNRamadan <edwardvijee@gmail.com>
2022-10-21 08:47:34 +0700

    Adding project from Spring Initializr and add some dependencies

---
+++ src/main/java/com/fikritech/simplecrudemployee/SimpleCrudEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.fikritech.simplecrudemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fikritech/simplecrudemployee/SimpleCrudEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fikritech.simplecrudemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
