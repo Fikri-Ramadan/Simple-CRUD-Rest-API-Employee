c422a93b78511ecad60b0fc7bf7c383051436564
FikriNRamadan <fikrinoviansyahramadan@gmail.com>
2022-10-27 18:04:15 +0700

    Working on repository package

--- src/main/java/com/fikritech/simplecrudemployee/repository/EmployeeRepository.java
+++ src/main/java/com/fikritech/simplecrudemployee/repository/EmployeeRepository.java
@@ -1,4 +1,12 @@
 package com.fikritech.simplecrudemployee.repository;
 
-public class EmployeeRepository {
+import com.fikritech.simplecrudemployee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+
 }
