5d89ebfbd46a97edc2ceb129cf5c9e08e57cb5c5
FikriNRamadan <fikrinoviansyahramadan@gmail.com>
2022-10-29 10:11:56 +0700

    Working on service package

--- src/main/java/com/fikritech/simplecrudemployee/service/EmployeeService.java
+++ src/main/java/com/fikritech/simplecrudemployee/service/EmployeeService.java
@@ -1,4 +1,17 @@
 package com.fikritech.simplecrudemployee.service;
 
-public class EmployeeService {
+import com.fikritech.simplecrudemployee.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface EmployeeService {
+
+    public Employee getOne(Long id);
+    public List<Employee> getAll();
+    public Employee addOne(Employee employee);
+    public Employee UpdateOne(Long id, Employee employee);
+    public Boolean deleteOne(Long id);
+
 }
--- src/main/java/com/fikritech/simplecrudemployee/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/fikritech/simplecrudemployee/service/impl/EmployeeServiceImpl.java
@@ -1,4 +1,56 @@
 package com.fikritech.simplecrudemployee.service.impl;
 
-public class EmployeeServiceImpl {
+import com.fikritech.simplecrudemployee.entity.Employee;
+import com.fikritech.simplecrudemployee.exception.ResourceNotFoundException;
+import com.fikritech.simplecrudemployee.repository.EmployeeRepository;
+import com.fikritech.simplecrudemployee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee getOne(Long id) {
+        return employeeRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException("Employee", "id", id));
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee addOne(Employee employee) {
+        Employee emp = Employee.builder()
+                .firstName(employee.getFirstName())
+                .lastName(employee.getLastName())
+                .email(employee.getEmail())
+                .build();
+
+        employeeRepository.save(emp);
+
+        return emp;
+    }
+
+    @Override
+    public Employee UpdateOne(Long id, Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteOne(Long id) {
+        boolean deleteStatus = false;
+
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            deleteStatus = true;
+        }
+
+        return deleteStatus;
+    }
 }
